467a69e6e170099619482d37aa75b47c7a981c75
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-04-19 03:52:51 -0400

    Update SearchEng.js

--- back_end/SearchEng.js
+++ back_end/SearchEng.js
@@ -1,21 +1,31 @@
 const express = require("express");
-const router = express.Router();
+const app = express();
+const port = 5432;
+
+app.set('view engine', 'ejs');
 
 
-router.get("/SearchEng", (req, res) => {
-    req.send({ data: "This is where all the data will be stored" });
+app.get("/", (req, res) => {
+    console.log("HomePage set-up");
+    res.send("This is the link towards the homepage");
 });
 
-router.post("/SearchEng", (req, res) => {
-    req.send({ data: "This will be set up for when a User is created" })
-});
+process.on('uncaughtException', err => {
+    console.error('There was an uncaught error: ' + (err)); 
+    process.exit(1); 
+   })
 
-router.put("/SearchEng", (req, res) => {
-    req.send({ data: "This will be where user account is updated" })
-});
+const userRouter = require("./routes/Users");
+const SearchEngRouter = require("./routes/SearchEng");
 
-router.delete("/SearchEng", (req, res) => {
-    req.send({ data: "This is where user is deleted" });
-});
+app.use("/Users", userRouter);
+app.use("/SearchEng", SearchEngRouter);
 
-module.exports = router;
+app.listen(port, (error) => {
+    if (error) {
+        console.log('Something went wrong', error);
+    }
+    else {
+        console.log("Server is running on port: " + port);
+    }
+});
