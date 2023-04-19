55258430b5bff910ccf0a2e8e7bbabd0769702a8
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-04-19 03:53:18 -0400

    Update SearchEng.js

--- back_end/SearchEng.js
+++ back_end/SearchEng.js
@@ -1,31 +1,18 @@
 const express = require("express");
-const app = express();
-const port = 5432;
-
-app.set('view engine', 'ejs');
+const router = express.Router();
 
 
-app.get("/", (req, res) => {
-    console.log("HomePage set-up");
-    res.send("This is the link towards the homepage");
+router.get("/", (req, res) => {
+    res.send("This is where all the data will be stored" );
 });
 
-process.on('uncaughtException', err => {
-    console.error('There was an uncaught error: ' + (err)); 
-    process.exit(1); 
-   })
-
-const userRouter = require("./routes/Users");
-const SearchEngRouter = require("./routes/SearchEng");
-
-app.use("/Users", userRouter);
-app.use("/SearchEng", SearchEngRouter);
-
-app.listen(port, (error) => {
-    if (error) {
-        console.log('Something went wrong', error);
-    }
-    else {
-        console.log("Server is running on port: " + port);
-    }
+router.get("/SearchEng/PreviousHistory", (req, res) => {
+    res.send("This will be if the user want to select a topic they search up prevgiously" )
 });
+
+router.post("/SearchEng/results", (req, res) => {
+    res.send("This will be where the user get results from search" )
+});
+
+module.exports = router;
+
