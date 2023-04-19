a62dddde7cb3b2f43647e553f7def9e642fb03c3
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-04-19 05:40:35 -0400

    Update User.js

--- back_end/User.js
+++ back_end/User.js
@@ -6,13 +6,33 @@ router.post("/", (req, res) => {
     //proccess of making a new user
 });
 
+// For now doing credential till i get it to look at different line for files in db
+const credentials = {
+    UserName: 'Admin',
+    Password: 'Jesus'
+};
+
 router.get("/login", (req, res) => {
     res.send("This is where all user will login to their account");
+    if (req.body.UserName == credentials.UserName && req.body.Password == credentials.Password) {
+        req.session.User = req.body.UserName;
+        res.redirect(/dashboard);
+
+    }
+    else {
+        res.end('Invalid UserName and Password.\nPlease try again');
+    }
     //Login screen to their account
 });
 
-router.get("/:id", (req, res) => {
+router.get("/dashboard/:id", (req, res) => {
     res.send("Get User with unique ID: ${req.params.id}");
+    if (req.session.user) {
+        res.render('dashboard', { user: req.session.user });
+    }
+    else {
+        res.send('Unauthorized User');
+    }
     //Basically their Account page
 });
 
@@ -39,6 +59,19 @@ router.put("/update/:id", (req, res) => {
 router.delete("delete/:id", (req, res) => {
     res.send("This is where the particular User ID: ${req.params.id}. \nWill be deleted");
     // If they wish to no longer have an account with us
+});
+
+router.get("/logout", (req, res) => {
+    res.send("This will be where user account is to sign out")
+    //Code to process finding their Password based off their unique Username
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('base', {title: 'Express'}, logout: 'logout completed successfully')
+        }
+    })
 });
 
 module.exports = router;
