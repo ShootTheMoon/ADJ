71d5129aafa438d736e3a0132bc96bc7adbc7ecb
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-04-19 03:52:10 -0400

    Update User.js

--- back_end/User.js
+++ back_end/User.js
@@ -1,21 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/User", (req, res) => {
-    req.send({ data: "This is where all the data will be stored" });
+router.post("/", (req, res) => {
+    res.send("Create new User here");
+    //proccess of making a new user
 });
 
-router.post("/User", (req, res) => {
-    req.send({ data: "This will be set up for when a User is created" })
+router.get("/login", (req, res) => {
+    res.send("This is where all user will login to their account");
+    //Login screen to their account
 });
 
-router.put("/User", (req, res) => {
-    req.send({ data: "This will be where user account is updated" })
+router.get("/:id", (req, res) => {
+    res.send("Get User with unique ID: ${req.params.id}");
+    //Basically their Account page
 });
 
-router.delete("/User", (req, res) => {
-    req.send({ data: "This is where user is deleted" });
+router.get("/forgotInfo", (req, res) => {
+    res.send("This will be where user account is forgot username/password")
+    //Here they choose if what they can't remember is a username or password the gets sent to new route'
+});
+
+router.get("/forgotInfo-UserName", (req, res) => {
+    res.send("This will be where user account is forgot username/password")
+    //Code to process finding their username based off their unique password
+});
+
+router.get("/forgotInfo-Password", (req, res) => {
+    res.send("This will be where user account is forgot username/password")
+    //Code to process finding their Password based off their unique Username
+});
+
+router.put("/update/:id", (req, res) => {
+    res.send("Update particular User ID: ${req.params.id}");
+    //If they wanna update their current password
+});
+
+router.delete("delete/:id", (req, res) => {
+    res.send("This is where the particular User ID: ${req.params.id}. \nWill be deleted");
+    // If they wish to no longer have an account with us
 });
 
 module.exports = router;
