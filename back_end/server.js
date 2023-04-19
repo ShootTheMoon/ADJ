711c41f5fa97959d9ec96f461184865d96cd97fd
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-04-19 03:51:17 -0400

    Update server.js

--- back_end/server.js
+++ back_end/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3123;
+const port = 5432;
 
-const userAccRoute = require("./User");
-const SearchEngRoute = require("./SearchEng");
+app.set('view engine', 'ejs');
 
-app.use('/User', userAccRoute);
-app.use('/SearchEng', SearchEngRoute);
+
+app.get("/", (req, res) => {
+    console.log("HomePage set-up");
+    res.send("This is the link towards the homepage");
+});
+
+process.on('uncaughtException', err => {
+    console.error('There was an uncaught error: ' + (err)); 
+    process.exit(1); 
+   })
+
+const userRouter = require("./routes/Users");
+const SearchEngRouter = require("./routes/SearchEng");
+
+app.use("/Users", userRouter);
+app.use("/SearchEng", SearchEngRouter);
 
 app.listen(port, (error) => {
     if (error) {
