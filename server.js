5aa889105f27a59890f25ad65e5615927a879c81
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-04-29 03:53:41 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const AuthRoute = require('./routes/auth')
+const SearchRoute = require('./routes/searchAuth')
+
+
+mongoose.connect('mongodb:localhost:27017/testdb')
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('Database Connection established')
+})
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const Port = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}')
+})
+
+app.use('./routes/auth', AuthRoute)
+app.use('./routes/searchAuth', SearchRoute)
