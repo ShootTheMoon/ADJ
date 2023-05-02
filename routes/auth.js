a2d6ec8c1697a4d52d0320fef22ab1302d1cc548
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-05-02 05:52:01 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const AuthController = require('../controller/UserController')
-const authenticate = require('../middleware/authenticate')
+// const authenticate = require('../middleware/authenticate')
 
 //Anything with authenticate in it will require a token to see in db
 
 
-router.get('/usersAll', authenticate, AuthController.showAll)
+router.get('/usersAll', AuthController.showAll)
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
 router.post('/update', AuthController.update)
