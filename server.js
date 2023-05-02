bcac6f28f92ca94356801bb38f22c38713403787
MaskedCrayon <131183684+MaskedCrayon@users.noreply.github.com>
2023-05-02 05:53:31 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,35 +2,39 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const home = require('./routes/home')
+
 const AuthRoute = require('./routes/auth')
 const SearchRoute = require('./routes/searchAuth')
+const home = require('./routes/home')
 
+const urL = 'mongodb+srv://MrBeast:6000@atlascluster.zpsuia0.mongodb.net/?retryWrites=true&w=majority'; // Put DB here
 
-mongoose.connect('mongodb:localhost:27017/testdb')
-const db = mongoose.connection
+async function connect() {
+    try {
+        await mongoose.connect(urL);
+        console.log('Connected to DB')
 
-db.on('error', (err) => {
-    console.log(err)
-})
+    } catch (error) {
+        console.error(error)
+    }
+}
 
-db.once('open', () => {
-    console.log('Database Connection established')
-})
-
-
+connect()
 const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const Port = process.env.PORT || 3000
+app.use(cors())
+const Port = 5000 //process.env.PORT ||
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}')
+app.listen(Port, () => {
+    console.log('Server is running on port', Port)
 })
+
 app.use('./routes/home', home)
 app.use('./routes/auth', AuthRoute)
 app.use('./routes/searchAuth', SearchRoute)
